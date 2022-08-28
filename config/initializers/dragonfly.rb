0d785f62be7284719cbbb668962f914da339b468
Andy Mayer <andy@yoomee.com>
2022-08-28 15:52:24 +0100

    Allow dev environment to also use an S3 bucket

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -11,19 +11,18 @@ Dragonfly.app.configure do
   secret '5c0fdd809b3682e9fbfc93b9d7fb4da7c839f9f4ad445ac022cc035fda0526ed'
 
   url_format "/media/:job/:name"
+ 
+  puts ""
+  puts "INFO: DragonFly is using S3_BUCKET_NAME: #{ENV['S3_BUCKET_NAME']}"
+  puts ""
 
-  if Rails.env.production?
-    datastore :s3,
-      bucket_name: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION'],
-      url_scheme: 'https'
-  else
-    datastore :file,
-      root_path: Rails.root.join('data', 'dragonfly', Rails.env),
-      server_root: Rails.root.join('public')
-  end
+  datastore :s3,
+    bucket_name: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    region: ENV['S3_REGION'],
+    url_scheme: 'https'
+
 
 end
 
