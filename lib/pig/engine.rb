3be8083bd5a145de05dd57df7c1f7de8543aaec6
Andy Mayer <andy@yoomee.com>
2024-07-15 17:22:53 +0100

    Refactor

--- lib/pig/engine.rb
+++ lib/pig/engine.rb
@@ -2,6 +2,16 @@ module Pig
   class Engine < ::Rails::Engine
     isolate_namespace Pig
 
+    ## Fix for Rails 7
+    initializer 'pig.load_helpers' do
+      # This ensures that we are targeting the engine's helpers directory
+      helpers_path = config.root.join('app', 'helpers', 'pig', '**', '*.rb')
+      Dir.glob(helpers_path).each do |file|
+        puts "DEBUG: require_dependency(#{file})" # For debugging purposes
+        require_dependency file
+      end
+    end
+
     config.to_prepare do
       Dir.glob(Rails.root + 'app/decorators/**/*_decorator*.rb').each do |c|
         require_dependency(c)
@@ -16,7 +26,7 @@ module Pig
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
 
-    initializer 'pig.action_controller' do
+    initializer 'pig.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         helper Pig::ApplicationHelper
         helper Pig::MetaTagsHelper
